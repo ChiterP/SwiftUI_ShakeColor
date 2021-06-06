81cd444590ec389b9573d7ca6f7a1265666f1704
ChiterP <63542970+ChiterP@users.noreply.github.com>
2021-06-06 23:42:35 +0300

    Initial Commit

---
+++ SwiftUI_ShakeColor/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_ShakeColor
+//
+//  Created by Алексей Сергейцев on 6/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_ShakeColor/SwiftUI_ShakeColorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ShakeColorApp.swift
+//  SwiftUI_ShakeColor
+//
+//  Created by Алексей Сергейцев on 6/6/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ShakeColorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
